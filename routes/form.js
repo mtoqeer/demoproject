65229f716b8c7404ca0035a7ae7a63b4f91785d6
Toqeer Iqbal <toqeer.94@gmail.com>
2020-02-12 14:28:26 +0500

    Update

--- routes/form.js
+++ routes/form.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const Form = require("../models/Form");
+const sgMail = require("@sendgrid/mail");
 
 // @route POST api/form
 // @desc Submit Form Data into DB
@@ -23,7 +24,9 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     const { name, email, telephone, subject, message } = req.body;
-
+    sgMail.setApiKey(
+      "SG.IF8NIhtoQ_uU95gy920mPA.BMgjoPukHg3z23F2v0ozm9u-Bikj2cOW2v2b2H-ktbo"
+    );
     try {
       const newFormData = new Form({
         name,
@@ -34,6 +37,18 @@ router.post(
       });
 
       const form = await newFormData.save();
+
+      // using Twilio SendGrid's v3 Node.js Library
+      // https://github.com/sendgrid/sendgrid-nodejs
+
+      const msg = {
+        to: "toqeer.94@gmail.com",
+        from: "toqeer.94@gmail.com",
+        subject: "Sending with Twilio SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+      };
+      sgMail.send(msg);
 
       res.json(form);
     } catch (e) {
