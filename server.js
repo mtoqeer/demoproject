fc2e25e6a2f3f2b0a42bfd73e39183f2bf3f629d
Toqeer Iqbal <toqeer.94@gmail.com>
2020-02-12 14:31:17 +0500

    update

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => res.json({ msg: "Welcome to the demo form" }));
 
 app.use("/api/form", require("./routes/form"));
 
+// serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started n port ${PORT}`));
